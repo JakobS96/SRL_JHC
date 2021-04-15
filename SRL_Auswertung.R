2da79426d496eb0b6b64d6936a4301dedab560b9
JakobS96 <jakobsierau@web.de>
2021-04-15 12:19:48 +0200

    Update SRL_Auswertung.R

--- SRL_Auswertung.R
+++ SRL_Auswertung.R
@@ -2,6 +2,8 @@ Alle_Daten <- read.csv2(file.choose())
 
 Bedingung <- read.csv2(file.choose())
 
+Bedingung <- Seriennummer_und_Bedingung
+
 # Kodierung fehlender Werte
 
 Alle_Daten[Alle_Daten == -9 | Alle_Daten == -1 | Alle_Daten == ""] <- NA
@@ -11,3 +13,12 @@ library("dplyr")
 # neue Variable: GROUP (Feedback = 1; Achtsamkeit = 0)
 
 Alle_Daten_neu <- left_join(Alle_Daten, Bedingung, by = "SERIAL")
+
+AD <- Alle_Daten_neu
+
+class(AD$TIME)
+
+AD$TIME_neu <- as.factor(AD$TIME)
+
+class(AD$TIME_neu)
+
